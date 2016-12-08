e2ea4025440b838bcf69b7a54af340412b76236d
Mark van Driel <mvdriel@nlware.com>
2016-12-08 20:10:02 +0100

    Day 07

---
+++ day07b.php
@@ -0,0 +1,22 @@
+<?php
+
+$input = file_get_contents('day07.txt');
+$lines = explode("\n", $input);
+
+$result = '';
+for($i=0;$i<strlen($lines[0]);$i++){
+  $letters=[];
+  foreach($lines as $line) {
+    if($line==='')break;
+    $l=substr($line,$i,1);
+    if(empty($letters[$l])) $letters[$l] = 0;
+    $letters[$l]++;
+  }
+
+  $k = array_keys($letters);
+  $v = array_values($letters);
+  array_multisort($v, SORT_ASC, $k, SORT_ASC);
+  $letters = array_combine($k, $v);
+  $result .= array_keys($letters)[0];
+}
+var_dump($result);
