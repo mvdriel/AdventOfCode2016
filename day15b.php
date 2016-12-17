31cbaa0dccd6fbf142e715dea466959f16424551
Mark van Driel <mvdriel@nlware.com>
2016-12-17 17:24:35 +0100

    Day 15

---
+++ day15b.php
@@ -0,0 +1,31 @@
+<?php
+
+$input=file_get_contents('day15.txt');
+$input=trim($input);
+$lines=explode("\n",$input);
+$discs=[];
+foreach($lines as $line){
+  $parts=explode(' ',$line);
+  $disc=[
+    'size'=>$parts[3],
+    'index'=>intval($parts[11])
+  ];
+  $discs[]=$disc;
+}
+$discs[]=['size'=>11,'index'=>0];
+
+for($i=0;true;$i++){
+  $success=true;
+  foreach($discs as $j=>$disc){
+    if((($disc['index']+$i+$j+1)%$disc['size'])!==0){
+      $success=false;
+      break;
+    }
+  }
+  if($success){
+    var_dump($i);
+    exit;
+  }
+}
+
+var_dump($discs);
